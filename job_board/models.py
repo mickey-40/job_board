dc3638656d747767e092135114cb224350c7d484
Arnold <A2461089@KATYISD.org>
2024-10-10 12:18:06 -0500

    added a model for job postings

--- job_board/models.py
+++ job_board/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+# model is just a python class
+# model represents a table in the db
+# attrs are the field on the table
+# job posting table
+#  with a title, description, company, salary
+
+class JobPosting(models.Model):
+    # id - starts at 1 and autoincrements
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+# makemigrations
+# -> creates instructions telling django how the db have changed
+# migrate
+# -> applies the above changes
