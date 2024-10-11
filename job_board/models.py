2316d325feeba60b3c74fc4e71cfab952fda8af2
Arnold <A2461089@KATYISD.org>
2024-10-11 06:52:41 -0500

    finished admin section

--- job_board/models.py
+++ job_board/models.py
@@ -15,6 +15,9 @@ class JobPosting(models.Model):
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.title} | {self.company} | Active: {self.is_active}"
+
 # makemigrations
 # -> creates instructions telling django how the db have changed
 # migrate
